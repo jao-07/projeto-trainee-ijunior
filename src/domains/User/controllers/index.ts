2c95c5c642beafb9c6097353d5f1083e002dd0a2
andresena15 <andresena1506@gmail.com>
2025-08-09 16:35:10 -0300

    Rotas envolvendo as primeiras 5 funções da classe artist

--- src/domains/User/controllers/index.ts
+++ src/domains/User/controllers/index.ts
@@ -1,8 +1,11 @@
 import { Router, Request, Response, NextFunction } from "express";
 import UserService from "../services/userService";
+import ArtistService  from "../../Artist/services/artistService";
+
 
 const router = Router();
 const userService = new UserService;
+const artistService = new ArtistService;
 
 router.get("/", async (req: Request, res: Response, next: NextFunction) => {
 	try{
@@ -65,6 +68,64 @@ router.delete("/:id", async (req: Request, res: Response, next: NextFunction) =>
 		next(error);
 	}
 });
+
+
+// Artist routes
+router.get("/artist", async (req: Request, res: Response, next: NextFunction) => {
+	try{
+		const artists = await artistService.getArtists();
+		res.json(artists);
+	}
+	catch (error) {
+		next(error);
+	}
+});
+
+router.get("/artist/:id", async (req: Request, res: Response, next: NextFunction) => {
+	try{
+		const artist = await artistService.getArtistByID(Number(req.params.id));
+		res.json(artist);
+	}
+	catch (error) {
+		next(error);
+	}
+});
+
+router.post("/artist", async (req: Request, res: Response, next: NextFunction) => {
+	try{
+		const data = req.body;
+		const artist = await artistService.createArtist(data);
+		res.json(artist);
+	}
+	catch (error){
+		next(error);
+	}
+});
+
+router.get("/artist/:name", async (req: Request, res: Response, next: NextFunction) => {
+	try{
+		const artist = await artistService.getArtistByName(req.params.name);
+		res.json(artist);
+	}
+	catch (error) {
+		next(error);
+	}
+});
+
+router.put("/artist/:id", async (req: Request, res: Response, next: NextFunction) => {
+	try{
+		const data = req.body;
+		const artist = await artistService.updateArtist(Number(req.params.id), data);
+		res.json(artist);
+	}
+	catch (error){
+		next(error);
+	}
+});
+
+
+
+
 
 
 
