88996c4db0fc3f3d76028d4e6d8f51242808e0b2
andresena15 <andresena1506@gmail.com>
2025-08-10 21:58:50 -0300

    Feat: Correções nas rotas do artista

--- src/domains/User/controllers/index.ts
+++ src/domains/User/controllers/index.ts
@@ -1,11 +1,10 @@
 import { Router, Request, Response, NextFunction } from "express";
 import UserService from "../services/userService";
-import ArtistService  from "../../Artist/services/artistService";
+
 
 
 const router = Router();
 const userService = new UserService;
-const artistService = new ArtistService;
 
 router.get("/", async (req: Request, res: Response, next: NextFunction) => {
 	try{
@@ -65,90 +64,6 @@ router.delete("/:id", async (req: Request, res: Response, next: NextFunction) =>
 		res.json(user);
 	}
 	catch (error){
-		next(error);
-	}
-});
-
-
-// Artist routes
-router.get("/artist", async (req: Request, res: Response, next: NextFunction) => {
-	try{
-		const artists = await artistService.getArtists();
-		res.json(artists);
-	}
-	catch (error) {
-		next(error);
-	}
-});
-
-router.get("/artist/:id", async (req: Request, res: Response, next: NextFunction) => {
-	try{
-		const artist = await artistService.getArtistByID(Number(req.params.id));
-		res.json(artist);
-	}
-	catch (error) {
-		next(error);
-	}
-});
-
-router.post("/artist", async (req: Request, res: Response, next: NextFunction) => {
-	try{
-		const data = req.body;
-		const artist = await artistService.createArtist(data);
-		res.json(artist);
-	}
-	catch (error){
-		next(error);
-	}
-});
-
-router.get("/artist/:name", async (req: Request, res: Response, next: NextFunction) => {
-	try{
-		const artist = await artistService.getArtistByName(req.params.name);
-		res.json(artist);
-	}
-	catch (error) {
-		next(error);
-	}
-});
-
-router.put("/artist/:id", async (req: Request, res: Response, next: NextFunction) => {
-	try{
-		const data = req.body;
-		const artist = await artistService.updateArtist(Number(req.params.id), data);
-		res.json(artist);
-	}
-	catch (error){
-		next(error);
-	}
-});
-
-router.delete("/artist/:id", async (req: Request, res: Response, next: NextFunction) => {
-	try{
-		const artist = await artistService.deleteByID(Number(req.params.id));
-		res.json(artist);
-	}
-	catch (error){
-		next(error);
-	}
-});
-
-router.put("/artist/:id/music/:musicID", async (req: Request, res: Response, next: NextFunction) => {
-	try{
-		const artist = await artistService.addMusicToArtist(Number(req.params.musicID), Number(req.params.id));
-		res.json(artist);
-	}
-	catch (error) {
-		next(error);
-	}
-});
-
-router.get("/artist/name/:name", async (req: Request, res: Response, next: NextFunction) => {
-	try{
-		const artist = await artistService.getArtistByName(req.params.name);
-		res.json(artist);
-	}
-	catch (error) {
 		next(error);
 	}
 });
