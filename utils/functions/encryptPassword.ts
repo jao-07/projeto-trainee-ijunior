ba06e43e8ef1340668f6a1c1bf8f519b7d8fb814
Joao Victor Evangelista Cruz <joaovecruz@yahoo.com.br>
2025-08-17 20:41:17 -0300

    feat: Função para comparar se a senha recebida é válida

--- utils/functions/encryptPassword.ts
+++ utils/functions/encryptPassword.ts
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt'
 
-async function encryptPassword(password: string) { //Função para criptografar uma senha
+async function encryptPassword(password: string) : Promise<string> { //Função para criptografar uma senha
     const saltRounds = 10;
     return await bcrypt.hash(password, saltRounds);
 }
