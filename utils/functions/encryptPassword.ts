90c393cf4d312aa4cc7563d9ae9c7cd3ad80396d
Joao Victor Evangelista Cruz <joaovecruz@yahoo.com.br>
2025-08-17 19:43:39 -0300

    feat: Adição da função de criptografia da senha do usuário

---
+++ utils/functions/encryptPassword.ts
@@ -0,0 +1,8 @@
+import bcrypt from 'bcrypt'
+
+async function encryptPassword(password: string) { //Função para criptografar uma senha
+    const saltRounds = 10;
+    return await bcrypt.hash(password, saltRounds);
+}
+
+export default encryptPassword
