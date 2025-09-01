c15a3d08149f908eb25aca312f6dd5c7d67a8e83
Joao Victor Evangelista Cruz <joaovecruz@yahoo.com.br>
2025-08-31 22:43:08 -0300

    Test(User): Testes de getUsers, getUserByID e update

--- src/domains/User/services/userService.test.ts
+++ src/domains/User/services/userService.test.ts
@@ -1,19 +1,17 @@
-jest.mock("../../../../utils/functions/encryptPassword", () => ({
-	__esModule: true,
-	default: jest.fn(),
-}));
-
 import { prismaMock } from "../../../../config/singleton";
 import UserService from "./userService";
 import { User } from "@prisma/client";
 import { userRoles } from "../../../../utils/constants/userRoles";
-import encryptPassword from "../../../../utils/functions/encryptPassword";
+import bcrypt from "bcrypt";
+import { QueryError } from "../../../../errors/QueryError";
 
 const userService = new UserService;
 
-describe("User: create", () => {
+jest.mock("bcrypt");
 
-	const userData: User = {
+describe("userService", () => {
+
+	const userData1: User = {
 		id: 1,
 		name: "joao",
 		email: "joao@email.com",
@@ -22,25 +20,140 @@ describe("User: create", () => {
 		privileges: userRoles.USER
 	};
 
-	test("deve criar o usuario quando não há um usuário já criado com o email passado", async () => {
-		prismaMock.user.findUnique.mockResolvedValue(null);
-		(encryptPassword as jest.Mock).mockResolvedValue(userData.password + "_encriptada");
-		prismaMock.user.create.mockResolvedValue(userData);
+	const userData2: User = {
+		id: 2,
+		name: "cecilia",
+		email: "cecilia@email.com",
+		photo: "photos/cecilia",
+		password: "cecilia123",
+		privileges: userRoles.ADMIN
+	};
 
-		const userCreated = await userService.create(userData);
+	(bcrypt.hash as jest.Mock).mockResolvedValue(userData1.password + "_encriptada");
 
-		expect(encryptPassword).toHaveBeenCalledWith("joao123");
-		expect(prismaMock.user.create).toHaveBeenCalledWith({
-			data: {
-				name: userData.name,
-				email: userData.email,
-				photo: userData.photo,
-				password: "joao123_encriptada",
-				privileges: userData.privileges
-			}
+	describe("create", () => {
+
+		test("deve criar o usuario quando não há um usuário já criado com o email passado", async () => {
+			prismaMock.user.findUnique.mockResolvedValue(null);
+			prismaMock.user.create.mockResolvedValue(userData1);
+
+			const userCreated = await userService.create(userData1);
+
+			expect(bcrypt.hash).toHaveBeenCalledWith("joao123", 10);
+			expect(prismaMock.user.create).toHaveBeenCalledWith({
+				data: {
+					name: userData1.name,
+					email: userData1.email,
+					photo: userData1.photo,
+					password: "joao123_encriptada",
+					privileges: userData1.privileges
+				}
+			});
+			expect(userCreated.id).toBe(1);
 		});
-		expect(userCreated.id).toBe(1);
+
+		test("deve gerar erro ao tentar criar um usuário com um email já existente no sistema", async () => {
+			prismaMock.user.findUnique.mockResolvedValue(userData1);
+			await expect(userService.create(userData1)).rejects.toThrow(QueryError);
+			expect(prismaMock.user.findUnique).toHaveBeenCalledWith({
+				where: { email: userData1.email },
+			});
+			expect(prismaMock.user.create).not.toHaveBeenCalled();
+		});
 	});
 
+	describe("getUsers", () => {
+		test("deve retornar todos os usuários cadastrados", async () => {
+			prismaMock.user.findMany.mockResolvedValue([userData1, userData2]);
+			const users = await userService.getUsers();
+			expect(users).toEqual([userData1, userData2]);
+			expect(prismaMock.user.findMany).toHaveBeenCalled();
+		});
 
+		test("deve retornar vazio quando não tiver usuários cadastrados", async () => {
+			prismaMock.user.findMany.mockResolvedValue([]);
+			const users = await userService.getUsers();
+			expect(users).toEqual([]);
+			expect(prismaMock.user.findMany).toHaveBeenCalled();
+		});
+	});
+
+	describe("getUserByID", () => {
+		test("deve retornar o usuário do ID passado", async () => {
+			prismaMock.user.findFirst.mockResolvedValue(userData2);
+			const user = await userService.getUserByID(2);
+			expect(user).toEqual(userData2);
+			expect(prismaMock.user.findFirst).toHaveBeenCalledWith({
+				where: {id: userData2.id}
+			});
+		});
+
+		test("deve retornar nulo quando não haver usuário com o ID passado", async () => {
+			prismaMock.user.findFirst.mockResolvedValue(null);
+			const user = await userService.getUserByID(3);
+			expect(user).toEqual(null);
+			expect(prismaMock.user.findFirst).toHaveBeenCalledWith({
+				where: {id: 3}
+			});
+		});
+	});
+
+	describe("update", () => {
+		test("deve atualizar os dados do usuário do ID passado, com as informações passadas, sem trocar senha", async () => {
+			const dataPassed = {
+				name: "Joao Victor",
+				photo: "photos/outraFoto"
+			};
+			const updatedUser = {...userData1, ...dataPassed};
+			prismaMock.user.update.mockResolvedValue(updatedUser);
+			const user = await userService.update(1, dataPassed);
+			expect(user).toEqual(updatedUser);
+			expect(prismaMock.user.update).toHaveBeenCalledWith({
+				data: dataPassed,
+				where: {
+					id: 1
+				}
+			});
+		});
+
+		test("deve encriptar a senha e atualizar o usuário", async () => {
+			const dataPassed = {
+				name: "Joao Victor",
+				password: "outraSenha123"
+			};
+			const updatedUser = {...userData1, ...dataPassed};
+			prismaMock.user.update.mockResolvedValue({...updatedUser, password: "outraSenha123_encriptada"});
+			const user = await userService.update(1, dataPassed);
+			expect(bcrypt.hash).toHaveBeenCalledWith("outraSenha123", 10);
+			expect(user).toEqual({...updatedUser, password: "outraSenha123_encriptada"});
+			expect(prismaMock.user.update).toHaveBeenCalledWith({
+				data: dataPassed,
+				where: {
+					id: 1
+				}
+			});
+		});
+
+		test("deve gerar erro ao passar um atributo inválido como parâmetro", async () => {
+			const dataPassed = {
+				name: "Joao Victor",
+				invalid: "invalid parameter"
+			};
+
+			prismaMock.user.update.mockRejectedValue(
+				new Error("Unknown arg `invalid` in data.invalid for type UserUpdateInput.")
+			);
+
+			await expect(userService.update(1, dataPassed))
+				.rejects
+				.toThrow("Unknown arg `invalid`");
+
+			expect(prismaMock.user.update).toHaveBeenCalledWith({
+				data: dataPassed,
+				where: {
+					id: 1
+				}
+			});
+		});
+	});
 });
